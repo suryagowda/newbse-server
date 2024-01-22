b990c1342c149da872edb7764ebe45b922891c79
Sudarshan <suryagowda590@gmail.com>
2024-01-22 23:44:42 +0530

    Initial commit

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -371,6 +371,7 @@ async function fetchAndShowStockData() {
       console.error('Error fetching and showing stock data:', error);
     }
 }
+git remote set-url origin https://github.com/suryagowda/newbse-server.git
 
 
 // Initialize the page by fetching top stocks and favorite stocks
